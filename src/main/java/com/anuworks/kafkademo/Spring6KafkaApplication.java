d7f929ccd8fe3027979408abe5e49feaec3872dd
Anudeep Madrampalli <anudeep571@gmail.com>
2025-03-31 19:01:08 +1100

    Initial commit

---
+++ src/main/java/com/anuworks/kafkademo/Spring6KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.anuworks.kafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6KafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuworks/kafkademo/Spring6KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuworks.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
